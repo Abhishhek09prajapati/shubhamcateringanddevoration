ae94e0e2359b651f1ce13656bd3c4262dea672cb
AbhishekSuraj0 <ab8948464510gmmail.com>
2026-03-22 10:36:14 +0530

    oki

--- index.js
+++ index.js
@@ -1,7 +1,50 @@
-function openMenu() {
-    document.getElementById("sidebar").style.left = "0";
-}
 
-function closeMenu() {
-    document.getElementById("sidebar").style.left = "-250px";
-}
+
+
+fetch('https://opensheet.elk.sh/1RKeR6fOsanl2ra79dYBemH8h4uAEZJdJBVjWX0QzSxk/WebsiteData')
+    .then(res => res.json())
+    .then(data => {
+        var slideimgpush = []
+
+        var servicsCardd = document.getElementById('servcis1')
+
+        data.forEach(servicsCard => {
+            
+            var div = document.createElement('div')
+            var image = document.createElement('img')
+            var span = document.createElement('span')
+            image.classList = "serrvicsimage"
+            div.classList = "servicsdata"
+
+            image.src = `./image/${servicsCard.imageServics}`
+            span.innerText = `${servicsCard.Servics} - Price:${servicsCard.Price} Upto`
+
+            div.appendChild(image)
+            div.appendChild(span)
+            servicsCardd.appendChild(div)
+        });
+
+
+
+        var slideimage = document.getElementById('sildeimg')
+
+        var i = 1
+        let columnLength = Object.keys(data[5]).length
+        console.log(columnLength)
+        setInterval(imageview, 500)
+
+        function imageview() {
+
+            if (i > columnLength) {
+                i=1
+            }
+            // console.log(i)
+            slideimage.src = `./image/${data[i].image}`
+
+            i++
+        }
+
+        
+
+
+    })
---
+++ others.js
@@ -0,0 +1,8 @@
+function openMenu() {
+    document.getElementById("sidebar").style.left = "0";
+}
+
+function closeMenu() {
+    document.getElementById("sidebar").style.left = "-250px";
+}
+
